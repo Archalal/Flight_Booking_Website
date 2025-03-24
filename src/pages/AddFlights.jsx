c3299aafc6da30efd4438498d0eacb127ac56a4c
Archa lal s <archa0002@gmail.com>
2025-03-24 22:34:05 +0530

    added price and add a new view for adding flights in flight dashboar

--- src/pages/AddFlights.jsx
+++ src/pages/AddFlights.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import FlightSidebar from './FlightSidebar'
 import TotalFlight from '../component/TotalFlight'
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
+import FlightAdded from './FlightAdded'
+
 
 
 
 const AddFlights = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+ 
   return (
     <div>
 
@@ -35,137 +29,10 @@ const AddFlights = () => {
   </div>
   <div className="col-10"  style={{ marginLeft: "16.65%", overflowX: "hidden" }}>
     <TotalFlight />
-    <div className="row">
-      <div className="col">
-        <h3>Add Flights</h3>
-      </div>
-      <div className="col">
-  <button className="round-button"
-  onClick={handleShow}><i className="fa-solid fa-plus"></i></button>
-  <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-
-          <div>
-          <FloatingLabel
-                    controlId="floatingSelect"
-                    label="Trip Type"
-                    className="mb-3"
-                  >
-                    <Form.Select 
-                      aria-label="Trip type select"
-                    value={""}
-                    >
-                      <option value="one-way">One Way</option>
-                      <option value="return">Return</option>
-                    </Form.Select>
-                  </FloatingLabel>
+    <FlightAdded />
+   
 
 
-          <FloatingLabel
-        controlId="floatingInput"
-        label="Name of the place"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="text" />
-      </FloatingLabel>
-
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Image of the place"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="text" />
-      </FloatingLabel>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="logo"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="text" />
-      </FloatingLabel>
-
-      <FloatingLabel
-        controlId="floatingInput"
-        label=" date of Departure"
-        className="mb-3"
-      >
-        <Form.Control type="date" placeholder="Departure" />
-      </FloatingLabel>
-
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Date of Arrival"
-        className="mb-3"
-      >
-        <Form.Control type="date" placeholder="Arrival" />
-      </FloatingLabel>
-
-      
-    
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Departure place"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="DeparturePlace" />
-      </FloatingLabel>
-
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Arrival place"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="ArrivalPlace" />
-      </FloatingLabel>
-
-      <FloatingLabel
-                    controlId="floatingSelect"
-                    label="Cabin Class"
-                    className="mb-3"
-                  >
-                    <Form.Select 
-                      aria-label="Cabin class select"
-                    value={""}
-                    >
-                      <option value="Business-class">Business class</option>
-                      <option value="Premium-class">Premium Class</option>
-                      <option value="Economy-class">Economy class</option>
-                    </Form.Select>
-                  </FloatingLabel>
-
-
-
-      <FloatingLabel
-        controlId="floatingInput"
-        label="No of Seats"
-        className="mb-3"
-      >
-        <Form.Control type="number" placeholder="Seats" />
-      </FloatingLabel>
-
-
-
-
-
-          </div>
-
-
-
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-</div>
-    </div>
     
     </div></div>       
       
